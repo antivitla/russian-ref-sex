9bb94c764e742e7874b9f67b8e612c00102633dd
Виталий Кулешин <antivitla@yandex.ru>
2022-12-31 21:53:19 +0300

    первый коммит

---
+++ sex.js
@@ -0,0 +1,12 @@
+export default {
+  'Мужчина': {
+    name: 'Мужчина',
+    latin: 'male',
+    regexp: /муж|дед/gi
+  },
+  'Женщина': {
+    name: 'Женщина',
+    latin: 'female',
+    regepx: /жен|баб/gi
+  }
+};
